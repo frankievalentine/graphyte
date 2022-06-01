68bebca7a5ec42acc9715fe80eccb77e759df843
Frankie Valentine <frankievalentine9@gmail.com>
2022-06-01 15:11:59 -0700

    scaffold the ghost content API

---
+++ pages/blog/posts.js
@@ -0,0 +1,15 @@
+import { getPosts } from "../../lib/posts";
+
+export async function getStaticProps(context) {
+  const posts = await getPosts();
+
+  if (!posts) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { posts },
+  };
+}
